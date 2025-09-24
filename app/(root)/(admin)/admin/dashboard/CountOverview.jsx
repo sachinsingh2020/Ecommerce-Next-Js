189df5e91905eff90aefadcaa70be3520685fd55
sachinsingh2021 <sachinsingh@astreait.com>
2025-09-24 13:19:01 +0530

    admin dashboard ui completed

--- app/(root)/(admin)/admin/dashboard/CountOverview.jsx
+++ app/(root)/(admin)/admin/dashboard/CountOverview.jsx
@@ -1,3 +1,10 @@
+"use client";
+import useFetch from "@/hooks/useFetch";
+import {
+  ADMIN_CATEGORY_SHOW,
+  ADMIN_CUSTOMERS_SHOW,
+  ADMIN_PRODUCT_SHOW,
+} from "@/routes/AdminPanelRoute";
 import Link from "next/link";
 import React from "react";
 import { BiCategory } from "react-icons/bi";
@@ -6,13 +13,17 @@ import { LuUserRound } from "react-icons/lu";
 import { MdOutlineShoppingBag } from "react-icons/md";
 
 const CountOverview = () => {
+  const { data: countData } = useFetch("/api/dashboard/admin/count");
   return (
     <div className="grid lg:grid-cols-4 sm:grid-cols-2 sm:gap-10 gap-5">
-      <Link href={""}>
+      <Link href={ADMIN_CATEGORY_SHOW}>
         <div className="flex items-center justify-between p-3 rounded-lg border shadow border-l-4 border-l-green-400 bg-white dark:bg-card dark:border-gray-800 dark:border-l-green-400">
           <div>
             <h4 className="font-medium text-gray-500">Total Categories</h4>
-            <span className="text-xl font-bold">10</span>
+            <span className="text-xl font-bold">
+              {" "}
+              {countData?.data?.category || 0}
+            </span>
           </div>
           <div>
             <span className="w-12 h-12 border flex justify-center items-center rounded-full bg-green-500 text-white">
@@ -21,11 +32,13 @@ const CountOverview = () => {
           </div>
         </div>
       </Link>
-      <Link href={""}>
+      <Link href={ADMIN_PRODUCT_SHOW}>
         <div className="flex items-center justify-between p-3 rounded-lg border shadow border-l-4 border-l-blue-400 bg-white dark:bg-card dark:border-gray-800 dark:border-l-blue-400">
           <div>
             <h4 className="font-medium text-gray-500">Total Products</h4>
-            <span className="text-xl font-bold">10</span>
+            <span className="text-xl font-bold">
+              {countData?.data?.product || 0}
+            </span>
           </div>
           <div>
             <span className="w-12 h-12 border flex justify-center items-center rounded-full bg-blue-500 text-white">
@@ -34,11 +47,13 @@ const CountOverview = () => {
           </div>
         </div>
       </Link>
-      <Link href={""}>
+      <Link href={ADMIN_CUSTOMERS_SHOW}>
         <div className="flex items-center justify-between p-3 rounded-lg border shadow border-l-4 border-l-yellow-400 bg-white dark:bg-card dark:border-gray-800 dark:border-l-yellow-400">
           <div>
             <h4 className="font-medium text-gray-500">Total Customers</h4>
-            <span className="text-xl font-bold">10</span>
+            <span className="text-xl font-bold">
+              {countData?.data?.customer || 0}
+            </span>
           </div>
           <div>
             <span className="w-12 h-12 border flex justify-center items-center rounded-full bg-yellow-500 text-white">
@@ -51,7 +66,7 @@ const CountOverview = () => {
         <div className="flex items-center justify-between p-3 rounded-lg border shadow border-l-4 border-l-cyan-400 bg-white dark:bg-card dark:border-gray-800 dark:border-l-cyan-400">
           <div>
             <h4 className="font-medium text-gray-500">Total Orders</h4>
-            <span className="text-xl font-bold">10</span>
+            <span className="text-xl font-bold">0</span>
           </div>
           <div>
             <span className="w-12 h-12 border flex justify-center items-center rounded-full bg-cyan-500 text-white">
