85ed61f6bd7a0e2dda43b51123efcf2c55b88dc3
sachinsingh2021 <sachinsingh@astreait.com>
2025-09-25 16:52:09 +0530

    making layout of website

--- app/(root)/(website)/layout.jsx
+++ app/(root)/(website)/layout.jsx
@@ -1,7 +1,22 @@
+import Footer from "@/components/Application/Website/Footer";
+import Header from "@/components/Application/Website/Header";
 import React from "react";
+import { Kumbh_Sans } from "next/font/google";
+
+const kumbh = Kumbh_Sans({
+  weight: ["400", "500", "600", "700", "800"],
+  display: "swap",
+  subsets: ["latin"],
+});
 
 const layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default layout;
