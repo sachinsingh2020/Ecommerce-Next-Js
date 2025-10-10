50a3372225795744c09f8df876c1c3470f4bcea7
sachinsingh2021 <sachinsingh@astreait.com>
2025-10-10 16:33:47 +0530

    home page completed

--- app/(root)/(website)/page.jsx
+++ app/(root)/(website)/page.jsx
@@ -6,6 +6,10 @@ import Image from "next/image";
 import Link from "next/link";
 import advertisingBanner from "@/public/assets/images/advertising-banner.png";
 import Testimonial from "@/components/Application/Website/Testimonial";
+import { GiReturnArrow } from "react-icons/gi";
+import { FaShippingFast } from "react-icons/fa";
+import { BiSupport } from "react-icons/bi";
+import { TbRosetteDiscountFilled } from "react-icons/tb";
 
 export default function Home() {
   return (
@@ -47,7 +51,39 @@ export default function Home() {
           width={advertisingBanner.width}
           alt="Advertisement"
         />
-        <Testimonial />
+      </section>
+      <Testimonial />
+      <section className="lg:px-32 px-4 border-t py-10">
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10">
+          <div className="text-center">
+            <p className="flex justify-center items-center mb-3">
+              <GiReturnArrow size={30} />
+            </p>
+            <h3 className="text-xl font-semibold">7-Days Returns</h3>
+            <p>Risk-free shopping with easy returns.</p>
+          </div>
+          <div className="text-center">
+            <p className="flex justify-center items-center mb-3">
+              <FaShippingFast size={30} />
+            </p>
+            <h3 className="text-xl font-semibold">Free Shipping</h3>
+            <p>No Extra Cost, just the price you see.</p>
+          </div>
+          <div className="text-center">
+            <p className="flex justify-center items-center mb-3">
+              <BiSupport size={30} />
+            </p>
+            <h3 className="text-xl font-semibold">24/7 Support</h3>
+            <p>24/7 support, always here just for you</p>
+          </div>
+          <div className="text-center">
+            <p className="flex justify-center items-center mb-3">
+              <TbRosetteDiscountFilled size={30} />
+            </p>
+            <h3 className="text-xl font-semibold">Member Discounts</h3>
+            <p>Special offers for our loyal customers</p>
+          </div>
+        </div>
       </section>
     </>
   );
