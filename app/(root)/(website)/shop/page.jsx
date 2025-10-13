9d8c72ce648d0453a14b7c371a571457e0fa8792
sachinsingh2021 <sachinsingh@astreait.com>
2025-10-13 17:19:51 +0530

    Filteration & sorting Completed

--- app/(root)/(website)/shop/page.jsx
+++ app/(root)/(website)/shop/page.jsx
@@ -1,6 +1,18 @@
+"use client";
 import Filter from "@/components/Application/Website/Filter";
 import WebsiteBreadcrumb from "@/components/Application/Website/WebsiteBreadcrumb";
 import { WEBSITE_SHOP } from "@/routes/WebsiteRoute";
+import Sorting from "@/components/Application/Website/Sorting";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import useWindowSize from "@/hooks/useWindowSize";
 
 const breadcrumb = {
   title: "Shop",
@@ -8,14 +20,48 @@ const breadcrumb = {
 };
 
 export default function ShopPage() {
+  const [limit, setLimit] = useState(9);
+  const [sorting, setSorting] = useState("default_sorting");
+  const [isMobileFilter, setIsMobileFilter] = useState(false);
+  const windowSize = useWindowSize();
+
   return (
     <div>
       <WebsiteBreadcrumb props={breadcrumb} />
       <section className="lg:flex lg:px-32 px-4 my-20">
-        <div className="w-72 me-4">
-          <div className="sticky top-0 bg-gray-50 p-4 rounded">
-            <Filter />
+        {windowSize.width > 1024 ? (
+          <div className="w-72 me-4">
+            <div className="sticky top-0 bg-gray-50 p-4 rounded">
+              <Filter />
+            </div>
           </div>
+        ) : (
+          <Sheet
+            open={isMobileFilter}
+            onOpenChange={() => setIsMobileFilter(false)}
+          >
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent side="left" className={"block"}>
+              <SheetHeader className={"border-b"}>
+                <SheetTitle>Filter</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
+              <div className="p-4 overflow-auto h-[calc(100vh-80px)]">
+                <Filter />
+              </div>
+            </SheetContent>
+          </Sheet>
+        )}
+
+        <div className="lg:w-[calc(100%-18rem)]">
+          <Sorting
+            limit={limit}
+            setLimit={setLimit}
+            sorting={sorting}
+            setSorting={setSorting}
+            mobileFilterOpen={isMobileFilter}
+            setMobileFilterOpen={setIsMobileFilter}
+          />
         </div>
       </section>
     </div>
