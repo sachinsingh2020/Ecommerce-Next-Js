3992d14c830a8f44a59101c040995f3075bf1e7d
sachinsingh2021 <sachinsingh@astreait.com>
2025-10-15 14:57:25 +0530

    in progress review & rating page

--- components/Application/Website/ProductReview.jsx
+++ components/Application/Website/ProductReview.jsx
@@ -1,16 +1,56 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
-import React from "react";
+import { zSchema } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { IoStar } from "react-icons/io5";
+import ButtonLoading from "../ButtonLoading";
+import { useSelector } from "react-redux";
+import { Rating } from "@mui/material";
+import { Textarea } from "@/components/ui/textarea";
 
-const ProductReview = ({ product }) => {
+const ProductReview = ({ productId }) => {
+  const auth = useSelector((store) => store.authStore.auth);
+  const [loading, setLoading] = useState(false);
+
+  const formSchema = zSchema.pick({
+    product: true,
+    userId: true,
+    rating: true,
+    title: true,
+    review: true,
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      product: productId,
+      userId: auth?._id,
+      rating: 0,
+      title: "",
+      review: "",
+    },
+  });
+
+  const handleReviewSubmit = (values) => {};
+
   return (
     <div className="shadow rounded border mb-20">
       <div className="p-3 bg-gray-50 border-b">
         <h2 className="font-semibold text-2xl">Rating & Reviews</h2>
       </div>
-      <div className="p-3">
+      <div className="p-5">
         <div className="flex justify-between flex-wrap items-center">
           <div className="md:w-1/2 w-full md:flex md:gap-10 md:mb-0 mb-5">
             <div className="md:w-[200px] w-full md:mb-0 mb-5">
@@ -27,7 +67,7 @@ const ProductReview = ({ product }) => {
             <div className="md:w-[calc(100%-200px)] flex items-center">
               <div className="w-full">
                 {[5, 4, 3, 2, 1].map((rating) => (
-                  <div className="flex items-center gap-2 mb-2">
+                  <div key={rating} className="flex items-center gap-2 mb-2">
                     <div className="flex items-center gap-1">
                       <p className="w-3">{rating}</p>
                       <IoStar />
@@ -48,6 +88,73 @@ const ProductReview = ({ product }) => {
               Write Review
             </Button>
           </div>
+        </div>
+
+        <div className="my-5 ">
+          <h4 className="text-xl font-semibold mb-3">Write a Review</h4>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleReviewSubmit)}>
+              <div className="mb-5">
+                <FormField
+                  control={form.control}
+                  name="rating"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Rating value={field.value} size="large" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mb-5">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input
+                          type={"text"}
+                          placeholder="Review Title"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mb-5">
+                <FormField
+                  control={form.control}
+                  name="review"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Review</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Write your comment here..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mb-3">
+                <ButtonLoading
+                  type="submit"
+                  text="Add Category"
+                  className={"cursor-pointer"}
+                  loading={loading}
+                />
+              </div>
+            </form>
+          </Form>
         </div>
       </div>
     </div>
